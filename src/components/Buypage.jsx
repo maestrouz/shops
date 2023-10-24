6587a2f4c62647023d3310c9c06757c8c1c7ed21
WebCodersDeveloper <ibrohimahmedov869@gmail.com>
2023-10-24 19:33:38 +0500

    edit

--- src/components/Buypage.jsx
+++ src/components/Buypage.jsx
@@ -8,13 +8,30 @@ export default function Buypage() {
   return (
 
     <>
-      <div className="container">
-        <div className="itemList">
-           {
-            buyers.map((item) => {
-              <img src={item.img} alt="" />
-            })
-           }
+      <div className="ghost__main">
+        <div id="background"></div>
+        <div class="top">
+          <h1>404</h1>
+          <h6>page not found</h6>
+        </div>
+        <div class="ghost_cont">
+          <div class="ghost-copy">
+            <div class="one"></div>
+            <div class="two"></div>
+            <div class="three"></div>
+            <div class="four"></div>
+          </div>
+          <div class="ghost">
+            <div class="face">
+              <div class="eye"></div>
+              <div class="eye-right"></div>
+              <div class="mouth"></div>
+            </div>
+          </div>
+          <div class="shadow"></div>
+        </div>
+        <div class="bottom">
+          <p>Bu qismni localstoragedagi muommolar tufayli yakunlay olmadim</p>
         </div>
       </div>
     </>
--- src/components/Like.jsx
+++ src/components/Like.jsx
@@ -1,7 +1,35 @@
 import React from 'react'
-
+import "./buypage.scss";
+import "./notfound.scss";
 export default function Like() {
   return (
-    <div>Like</div>
-  )
+    <>
+    <div className="ghost__main">
+        <div id="background"></div>
+        <div class="top">
+          <h1>404</h1>
+          <h6>page not found</h6>
+        </div>
+        <div class="ghost_cont">
+          <div class="ghost-copy">
+            <div class="one"></div>
+            <div class="two"></div>
+            <div class="three"></div>
+            <div class="four"></div>
+          </div>
+          <div class="ghost">
+            <div class="face">
+              <div class="eye"></div>
+              <div class="eye-right"></div>
+              <div class="mouth"></div>
+            </div>
+          </div>
+          <div class="shadow"></div>
+        </div>
+        <div class="bottom">
+          <p>Bu qismni localstoragedagi muommolar tufayli yakunlay olmadim</p>
+        </div>
+      </div>
+    </>
+  );
 }
